390c9bbb67aff235e2a8ea8f09a06e2931b52812
Thành Tấn Trương <truongtanthanh1202@gmail.com>
2021-12-07 22:05:58 +0700

    commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -21,15 +21,6 @@
 function bodyStopScrolling() {
     document.body.classList.toggle("stop-scrolling");
 }
-/*------------------Show and hide header shadow when scroll up and down--------------------------------*/
-// (()=>{
-//     // adding scroll event
-//     window.addEventListener('scroll', function(){
-//         // detects new state and compares it with the new one
-//         if (document.body.scrollTop > 0) {
-//             document.querySelector(".header").classList.add("header-shadow");
-//         }else{
-//             document.querySelector(".header").classList.remove("header-shadow");
-//         }
-//     });
-// }) ();
+/*-------------------------------------------------
+Show and hide header shadow when scroll up and down
+--------------------------------------------------*/
