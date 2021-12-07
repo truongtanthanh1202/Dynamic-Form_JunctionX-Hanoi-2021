f997ac5c2d9e8c3104a2543baed7b1c6a66d4cce
Thành Tấn Trương <truongtanthanh1202@gmail.com>
2021-12-07 22:46:59 +0700

    commit

--- asset/js/form.js
+++ asset/js/form.js
@@ -1,4 +1,32 @@
 /*-------------------
     Form.html
 ---------------------*/ 
-console.log("Helloo");
+console.log("Helloo Hải đần");
+const formQuestionBody = document.getElementById("form-question--body");
+
+/*---------------Add and delete inputBox-----------*/ 
+(() => {
+    const addInput = document.querySelector(".add-input");
+
+    addInput.addEventListener("click", () => {
+        createInputBox();
+    })
+
+    let i=1;
+    function createInputBox() {
+        console.log(formQuestionBody);
+        inputBox = document.createElement("div");
+
+        inputBox.classList.add("form-question--inputBox-group");
+        inputBox.innerHTML=`
+        <div class="form-question--inputBox w-100 form-control--container">
+            <button type="button" class="btn--remove-asw-field">
+                <i class="fas fa-trash-alt"></i>
+            </button>
+            <input type="text" name="input-descript" id="input-descript" class="form-control--descript" value="Input box description">
+            <input type="text" name="user-${i}-inputBox" id="user-${i}-inputBox" class="form-control" required>
+        </div>`;
+        i++;
+        formQuestionBody.appendChild(inputBox);
+    }
+})();
