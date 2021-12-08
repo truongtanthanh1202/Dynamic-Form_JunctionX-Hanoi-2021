30295015a4410faf7a4eee236ee98f7ed67fe411
Thành Tấn Trương <truongtanthanh1202@gmail.com>
2021-12-08 11:17:10 +0700

    commit

--- asset/js/form.js
+++ asset/js/form.js
@@ -7,17 +7,12 @@ const formQuestionBody = document.getElementById("form-question--body");
 (() => {
     /*---Add---*/ 
     const addInputBox = document.querySelector(".add-input");
-    var btns = document.querySelectorAll(".btn--remove-input");
 
     addInputBox.addEventListener("click", () => {
         createInputBox();
-        //lưu lại xem có bao nhiêu nút xóa của inputBox
-        btns = document.querySelectorAll(".btn--remove-input");
-        console.log(btns);
-        console.log(btns.length);
     })
 
-    let i=1;
+    let i=0;
     function createInputBox() {
         inputBox = document.createElement("div");
 
@@ -26,8 +21,7 @@ const formQuestionBody = document.getElementById("form-question--body");
         inputBox.innerHTML=`
         <div class="row">
             <div class="form-question--inputBox w-100 form-control--container">
-                <button type="button" class="btn--remove-asw-field btn--remove-input" id="btn--remove-input-${i}">
-                    <i class="fas fa-trash-alt"></i>
+                <button type="button" class="btn--remove-asw-field btn--remove-input fas fa-trash-alt" id="btn--remove-input-${i}">
                 </button>
                 <input type="text" name="input-descript" id="input-descript" class="form-control--descript" value="Input box description">
                 <input type="text" name="user-${i}-inputBox" id="user-${i}-inputBox" class="form-control" required>
@@ -35,17 +29,27 @@ const formQuestionBody = document.getElementById("form-question--body");
         </div>`;
         i++;
         formQuestionBody.appendChild(inputBox);
+        reload();
     }
 
     /*---Remove---*/
-    const removeInputBox = document.querySelector("#btn--remove-input-0");
 
-    removeInputBox.addEventListener("click", () => {
-        deleteInputBox();
-    })
-
-    function deleteInputBox() {
-        var child69 = document.getElementById("form-question--inputBox-group-0");
-        formQuestionBody.removeChild(child69);
+    function deleteInputBox(event) {
+        console.log("hello");
+        console.log(event.target);
+        const currentContainer = [...document.querySelectorAll(".form-question--inputBox-group")].find(
+            (container) =>
+                container.id.replace("form-question--inputBox-group-", "") ===
+                event.target.id.replace("btn--remove-input-", "")
+        );
+        currentContainer.remove();
+        reload();
     }
+
+    const reload = () => {
+        [...document.querySelectorAll(".btn--remove-input")].forEach((btn) => {
+            btn.addEventListener("click", deleteInputBox);
+        });
+    };
+    // reload();
 })();
