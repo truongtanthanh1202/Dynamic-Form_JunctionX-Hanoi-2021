d9e3167151d76196bd96c126bd873d6f4e12da04
Thành Tấn Trương <truongtanthanh1202@gmail.com>
2021-12-09 01:56:16 +0700

    commit

--- asset/js/form.js
+++ asset/js/form.js
@@ -15,9 +15,61 @@ const formQuestionBody = document.getElementById("form-question--body");
     function createPersonInfoGroup(){
         PersonInfoGroup = document.createElement("div");
 
-        PersonInfoGroup.classList.add("form-question--checkbox-group");
-        PersonInfoGroup.setAttribute("id", "form-question--checkbox-group-"+(i))
-        PersonInfoGroup.innerHTML=``;
+        PersonInfoGroup.classList.add("form-question--personal-info-group");
+        PersonInfoGroup.setAttribute("id", "form-question--personal-info-group-"+(i))
+        PersonInfoGroup.innerHTML=`
+        <div class="row">
+        <!-- name -->
+            <div id="form-question--fullName-${i}" class="form-question--fullName w-50 form-control--container">
+                <button type="button" id="btn--remove-fullName-${i}" class="btn--remove-fullName btn--remove-asw-field fas fa-trash-alt">
+                </button>
+                <input type="text" name="input-descript" id="input-descript" class="form-control--descript" value="Full name">
+                <input type="text" name="user-${i}-fullName" id="user-${i}-fullName" class="form-control" required>
+            </div>
+            <!-- email -->
+            <div id="form-question--email-${i}" class="form-question--email w-50 form-control--container">
+                <button type="button" id="btn--remove-email-${i}" class="btn--remove-email btn--remove-asw-field fas fa-trash-alt">
+                </button>
+                <input type="text" name="input-descript" id="input-descript" class="form-control--descript" value="Email">
+                <input type="email" name="user-${i}-email" id="user-${i}-email" class="form-control" placeholder="email@domain.com" required>
+            </div>
+            <!-- birthday -->
+            <div id="form-question--birthday-${i}" class="form-question--birthday w-50 form-control--container">
+                <button type="button" id="btn--remove-birthday-${i}" class="btn--remove-birthday btn--remove-asw-field fas fa-trash-alt">
+                </button>
+                <input type="text" name="input-descript" id="input-descript" class="form-control--descript" value="Birthday">
+                <input type="date" name="user-${i}-birthday" id="user-${i}-birthday" class="form-control" required>
+            </div>
+            <!-- phone -->
+            <div id="form-question--phone-${i}" class="form-question--phone w-50 form-control--container">
+                <button type="button" id="btn--remove-phone-${i}" class="btn--remove-phone btn--remove-asw-field fas fa-trash-alt">
+                </button>
+                <input type="text" name="input-descript" id="input-descript" class="form-control--descript" value="Phone number">
+                <input type="tel" name="user-${i}-phone" id="user-${i}-phone" class="form-control" pattern="[0-9]{10}" placeholder="0123456789" required>
+            </div>
+            <!-- address -->
+            <div id="form-question--address-${i}" class="form-question--address w-100 form-control--container">
+                <button type="button" id="btn--remove-address-${i}" class="btn--remove-address btn--remove-asw-field fas fa-trash-alt">
+                </button>
+                <input type="text" name="input-descript" id="input-descript" class="form-control--descript" value="Address">
+                <input type="text" name="user-${i}-address" id="user-${i}-address" class="form-control" placeholder="Street - City - Country" required>
+            </div>
+            <!-- CCCD -->
+            <div id="form-question--CCCD-${i}" class="form-question--CCCD w-50 form-control--container">
+                <button type="button" id="btn--remove-CCCD-${i}" class="btn--remove-CCCD btn--remove-asw-field fas fa-trash-alt">
+                </button>
+                <input type="text" name="input-descript" id="input-descript" class="form-control--descript" value="Cityzen Indentify number">
+                <input type="number" name="user-${i}-CCCD" id="user-${i}-CCCD" class="form-control" required>
+            </div>
+            <!-- works -->
+            <div id="form-question--works-${i}" class="form-question--works w-100 form-control--container">
+                <button type="button" id="btn--remove-works-${i}" class="btn--remove-works btn--remove-asw-field fas fa-trash-alt">
+                </button>
+                <input type="text" name="input-descript" id="input-descript" class="form-control--descript" value="Works">
+                <input type="text" name="user-${i}-works" id="user-${i}-works" class="form-control" placeholder="Company Name" required>
+            </div>
+            <div class="form--endline--inner"></div>
+        </div>`;
         i++;
         formQuestionBody.appendChild(PersonInfoGroup);
         reload();
@@ -25,10 +77,101 @@ const formQuestionBody = document.getElementById("form-question--body");
 
     /*---Reload and add Click event for many btn-remove*/
     const reload = () => {
-        [...document.querySelectorAll(".btn--remove-checkbox")].forEach((btn) => {
-            btn.addEventListener("click", deleteCheckBox);
+        [...document.querySelectorAll(".btn--remove-fullName")].forEach((fullName) => {
+            fullName.addEventListener("click", deleteFullName);
         });
-    }; 
+        [...document.querySelectorAll(".btn--remove-email")].forEach((email) => {
+            email.addEventListener("click", deleteEmail);
+        });
+        [...document.querySelectorAll(".btn--remove-birthday")].forEach((birthday) => {
+            birthday.addEventListener("click", deleteBirthday);
+        });
+        [...document.querySelectorAll(".btn--remove-phone")].forEach((phone) => {
+            phone.addEventListener("click", deletePhone);
+        });
+        [...document.querySelectorAll(".btn--remove-address")].forEach((address) => {
+            address.addEventListener("click", deleteAddress);
+        });
+        [...document.querySelectorAll(".btn--remove-CCCD")].forEach((cccd) => {
+            cccd.addEventListener("click", deleteCCCD);
+        });
+        [...document.querySelectorAll(".btn--remove-works")].forEach((works) => {
+            works.addEventListener("click", deleteWorks);
+        });
+    };
+    reload();
+    
+    /*---Remove element item----*/
+    // Remove fullName
+    function deleteFullName(event) {
+        const currentContainer = [...document.querySelectorAll(".form-question--fullName")].find(
+            (container) =>
+                container.id.replace("form-question--fullName-", "") ===
+                event.target.id.replace("btn--remove-fullName-", "")
+        );
+        currentContainer.remove();
+        reload();
+    }
+    // Remove email
+    function deleteEmail(event) {
+        const currentContainer = [...document.querySelectorAll(".form-question--email")].find(
+            (container) =>
+                container.id.replace("form-question--email-", "") ===
+                event.target.id.replace("btn--remove-email-", "")
+        );
+        currentContainer.remove();
+        reload();
+    } 
+    // Remove birthday
+    function deleteBirthday(event) {
+        const currentContainer = [...document.querySelectorAll(".form-question--birthday")].find(
+            (container) =>
+                container.id.replace("form-question--birthday-", "") ===
+                event.target.id.replace("btn--remove-birthday-", "")
+        );
+        currentContainer.remove();
+        reload();
+    } 
+    // Remove phone
+    function deletePhone(event) {
+        const currentContainer = [...document.querySelectorAll(".form-question--phone")].find(
+            (container) =>
+                container.id.replace("form-question--phone-", "") ===
+                event.target.id.replace("btn--remove-phone-", "")
+        );
+        currentContainer.remove();
+        reload();
+    }
+    // Remove address
+    function deleteAddress(event) {
+        const currentContainer = [...document.querySelectorAll(".form-question--address")].find(
+            (container) =>
+                container.id.replace("form-question--address-", "") ===
+                event.target.id.replace("btn--remove-address-", "")
+        );
+        currentContainer.remove();
+        reload();
+    } 
+    // Remove CCCD
+    function deleteCCCD(event) {
+        const currentContainer = [...document.querySelectorAll(".form-question--CCCD")].find(
+            (container) =>
+                container.id.replace("form-question--CCCD-", "") ===
+                event.target.id.replace("btn--remove-CCCD-", "")
+        );
+        currentContainer.remove();
+        reload();
+    } 
+    // Remove works
+    function deleteWorks(event) {
+        const currentContainer = [...document.querySelectorAll(".form-question--works")].find(
+            (container) =>
+                container.id.replace("form-question--works-", "") ===
+                event.target.id.replace("btn--remove-works-", "")
+        );
+        currentContainer.remove();
+        reload();
+    } 
 })();
 
 /*---------------Add and delete Question Title-----------*/ 
